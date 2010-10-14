f2d79baeb11487be6de49fbd61e09c17c4b814b1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-14 19:35:26 +0200

    Generate rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnyt::Application.config.secret_token = '1afbfff9e3fa22c9a109d9a38d46bc36d2e98c7e4e3e722e3209ad2d676a5a1b86ba27c65dcd8e8fca4bcd1ff1903a0743f4a0184bb35eb66f2fe2cf00a23a40'
