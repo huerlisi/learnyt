af8b78b9c4c80da9ae171a6880c732e2f9cc95cf
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-15 22:09:03 +0200

    Use contextual helper and add ajax add to answers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def index_contextual_for(model)
+    model_name = model.to_s.underscore
+    
+    render 'layouts/index_contextual_for', :model => model, :model_name => model_name
+  end
 end
