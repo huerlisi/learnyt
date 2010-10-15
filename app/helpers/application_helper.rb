7e941c7469ea1b5520dfd2855b9763b4f8dbae00
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-15 23:08:38 +0200

    New helper list_item_actions_for, port possible_answer list.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,12 @@ module ApplicationHelper
     render 'layouts/index_contextual_for', :model => model, :model_name => model_name
   end
 
+  def list_item_actions_for(resource)
+    model_name = resource.class.to_s.underscore
+    
+    render 'layouts/list_item_actions_for', :model_name => model_name, :resource => resource
+  end
+
   def show_new_form(model)
     model_name = model.to_s.underscore
     
