04b3e40e69332330f467108aea2bbdf2be2c2137
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-15 22:54:07 +0200

    New helper show_new_form, use in answers/new.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,15 @@ module ApplicationHelper
     
     render 'layouts/index_contextual_for', :model => model, :model_name => model_name
   end
+
+  def show_new_form(model)
+    model_name = model.to_s.underscore
+    
+    output = <<EOF
+$('##{model_name}_list').before('#{escape_javascript(render('form'))}')
+addAutofocusBehaviour()
+EOF
+  
+    return output.html_safe
+  end
 end
