c5c98dcbc33e6934c3b07911892f5ca4669af49b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-17 18:05:13 +0200

    Use javascript to update correctness styling in form; fix sorting in question form.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,7 +9,32 @@ selectables.click(function(element) {
 });
 
 $(".sortable").bind('sortupdate', function(event, ui) {
-  $(this).find('fieldset.possible_answer').each(function(index, element) {
+  $(this).find('li.possible_answer').each(function(index, element) {
     $(this).find("input[id$='_position']").val(index + 1)
   });
+});
+
+function setCorrectness(element) {
+  var container = element.closest('li');
+  var score = element.val();
+
+  var correctness = 'close';
+  if (score == 1.0) {
+    correctness = 'correct';
+  } else if (score == 0.0) {
+    correctness = 'wrong';
+  };
+
+  element.removeClass('close correct wrong');
+  container.removeClass('close correct wrong');
+  element.addClass(correctness);
+  container.addClass(correctness);
+}
+
+$("form .score input").change(function(event) {
+  setCorrectness($(this));
+});
+
+$("form .score input").each(function() {
+  setCorrectness($(this));
 });
