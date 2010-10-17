62780b700f6048cc9eb1aed50a3952503a0626e4
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-17 13:33:52 +0200

    Use pointer cursor for selectable answers.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,8 @@
 // learnyt
 
-$('#response form .possible_answer').click(function(element) {
+var selectables = $('#response form .possible_answer');
+selectables.css('cursor', "pointer");
+selectables.click(function(element) {
   $('#response_possible_answer_id').val($(this).data('id'));
   $('.possible_answer').removeClass('selected');
   $(this).addClass('selected');
