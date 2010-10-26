0872adbcb65411c252fab7713cdd5dc6d1afce85
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-26 10:33:38 +0200

    Move correctness indicator behaviour to function.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,30 +14,33 @@ $(".sortable").bind('sortupdate', function(event, ui) {
   });
 });
 
-function setCorrectness(element) {
-  var container = element.closest('li');
-  var score = element.val();
+// Correctness indicators
+function addCorrectnessIndicatorBehaviour() {
+  function setCorrectness(element) {
+    var container = element.closest('li');
+    var score = element.val();
 
-  var correctness = 'close';
-  if (score == 1.0) {
-    correctness = 'correct';
-  } else if (score == 0.0) {
-    correctness = 'wrong';
-  };
+    var correctness = 'close';
+    if (score == 1.0) {
+      correctness = 'correct';
+    } else if (score == 0.0) {
+      correctness = 'wrong';
+    };
 
-  element.removeClass('close correct wrong');
-  container.removeClass('close correct wrong');
-  element.addClass(correctness);
-  container.addClass(correctness);
-}
+    element.removeClass('close correct wrong');
+    container.removeClass('close correct wrong');
+    element.addClass(correctness);
+    container.addClass(correctness);
+  }
 
-$("form .score input").change(function(event) {
-  setCorrectness($(this));
-});
+  $("form .score input").change(function(event) {
+    setCorrectness($(this));
+  });
 
-$("form .score input").each(function() {
-  setCorrectness($(this));
-});
+  $("form .score input").each(function() {
+    setCorrectness($(this));
+  });
+};
 
 // Nested Forms
 function addNestedFormsBehaviour() {
@@ -95,4 +98,5 @@ function addAutocompleteBehaviour() {
 $(document).ready(function() {
     addAutocompleteBehaviour();
     addNestedFormsBehaviour();
+    addCorrectnessIndicatorBehaviour();
 });
