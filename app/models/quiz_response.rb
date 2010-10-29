69371073e359562ed843a9c3e7e1be5e472b573e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-29 11:35:29 +0200

    Show score in response list.

--- app/models/quiz_response.rb
+++ app/models/quiz_response.rb
@@ -3,4 +3,25 @@ class QuizResponse < ActiveRecord::Base
   belongs_to :quiz
   has_many :responses
   accepts_nested_attributes_for :responses
+
+  # Helpers
+  def to_s
+    "%0.1f out of %0.1f points" % [score, max_score]
+  end
+
+  # Calculated fields
+  
+  # Calculate overal score.
+  #
+  # Simply add up the scores for all responses.
+  def score
+    responses.map{|response| response.score}.inject(:+)
+  end
+
+  # Calculate maximum score.
+  #
+  # Simply use the response count * 1.
+  def max_score
+    responses.count
+  end
 end
