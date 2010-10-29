652a7e41d97ad4ab2c2ea1a2ab25dba2aaf711c7
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-29 11:22:03 +0200

    Move quiz response to main navigation, drop answers from there.

--- config/navigation/main_navigation.rb
+++ config/navigation/main_navigation.rb
@@ -70,14 +70,14 @@ SimpleNavigation::Configuration.run do |navigation|
       question.item :question_index, t_action(:index, Question), questions_path
     end
 
-    primary.item :nav_answers, t_model(Answer), answers_path, :highlights_on => /\/answers/ do |answer|
-      answer.item :new_answer, t_action(:new, Answer), new_answer_path
-      answer.item :answer_index, t_action(:index, Answer), answers_path
-    end
-
     primary.item :nav_quizzes, t_model(Quiz), quizzes_path, :highlights_on => /\/quizzes/ do |quiz|
       quiz.item :new_quiz, t_action(:new, Quiz), new_quiz_path
       quiz.item :quiz_index, t_action(:index, Quiz), quizzes_path
+    end
+
+    primary.item :nav_quiz_responses, t_model(QuizResponse), quiz_responses_path, :highlights_on => /\/quiz_responses/ do |quiz_response|
+      quiz_response.item :new_quiz_response, t_action(:new, QuizResponse), new_quiz_response_path
+      quiz_response.item :quiz_response_index, t_action(:index, QuizResponse), quiz_responses_path
     end
 
     primary.item :nav_settings, t('main_navigation.settings'), edit_user_registration_path, :highlights_on => /\/users\/edit/
