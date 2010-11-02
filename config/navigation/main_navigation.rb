a6ea392e14a7ec49374d31c458b2430d2a0bd88b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-02 07:48:33 +0100

    Move quiz to first place in navigation.

--- config/navigation/main_navigation.rb
+++ config/navigation/main_navigation.rb
@@ -5,14 +5,14 @@ SimpleNavigation::Configuration.run do |navigation|
     # mailyt navigation
     primary.item :overview, t('main_navigation.overview'), root_path
     
-    primary.item :nav_questions, t_model(Question), questions_path do |question|
-      question.item :new_question, t_action(:new, Question), new_question_path
-      question.item :question_index, t_action(:index, Question), questions_path
-    end
-
     primary.item :nav_quizzes, t_model(Quiz), quizzes_path do |quiz|
       quiz.item :new_quiz, t_action(:new, Quiz), new_quiz_path
       quiz.item :quiz_index, t_action(:index, Quiz), quizzes_path
+    end
+
+    primary.item :nav_questions, t_model(Question), questions_path do |question|
+      question.item :new_question, t_action(:new, Question), new_question_path
+      question.item :question_index, t_action(:index, Question), questions_path
     end
 
     primary.item :nav_quiz_responses, t_model(QuizResponse), quiz_responses_path do |quiz_response|
