39f5785d70e4010bb9547cdbef3ab6bc50e438e1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-05 13:51:44 +0100

    Only show navigational items available for current role.

--- config/navigation/main_navigation.rb
+++ config/navigation/main_navigation.rb
@@ -6,17 +6,17 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :overview, t('main_navigation.overview'), root_path
     
     primary.item :nav_quizzes, t_model(Quiz), quizzes_path do |quiz|
-      quiz.item :new_quiz, t_action(:new, Quiz), new_quiz_path
+      quiz.item :new_quiz, t_action(:new, Quiz), new_quiz_path, :if => lambda { can?(:new, Quiz)}
       quiz.item :quiz_index, t_action(:index, Quiz), quizzes_path
     end
 
-    primary.item :nav_questions, t_model(Question), questions_path do |question|
-      question.item :new_question, t_action(:new, Question), new_question_path
-      question.item :question_index, t_action(:index, Question), questions_path
+    primary.item :nav_questions, t_model(Question), questions_path, :if => lambda { can?(:new, Question) or can?(:index, Question)} do |question|
+      question.item :new_question, t_action(:new, Question), new_question_path, :if => lambda { can?(:new, Question)}
+      question.item :question_index, t_action(:index, Question), questions_path, :if => lambda { can?(:index, Question)}
     end
 
     primary.item :nav_quiz_responses, t_model(QuizResponse), quiz_responses_path do |quiz_response|
-      quiz_response.item :new_quiz_response, t_action(:new, QuizResponse), new_quiz_response_path
+      quiz_response.item :new_quiz_response, t_action(:new, QuizResponse), new_quiz_response_path, :if => lambda { can?(:new, QuizResponse)}
       quiz_response.item :quiz_response_index, t_action(:index, QuizResponse), quiz_responses_path
     end
 
