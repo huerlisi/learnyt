62ad09cf8112b3c7bed221dc68c68ee70557f57d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-08 23:11:53 +0100

    No need to fill in all possible answers for questions, just take what is provided.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   # Associations
   has_many :possible_answers, :order => :position
-  accepts_nested_attributes_for :possible_answers, :allow_destroy => true
+  accepts_nested_attributes_for :possible_answers, :allow_destroy => true, :reject_if => lambda {|attributes| attributes[:answer_attributes][:title].blank?}
   
   has_many :answers, :through => :possible_answers
   accepts_nested_attributes_for :answers
