b6fea2bcfbf88dbfc127fc71393f8eb5453bb310
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-09 10:47:52 +0100

    Handle user updates with no password changes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < AuthorizedController
+  def update
+    @user = User.find(params[:id])
+    
+    if params[:user][:password].blank?
+      [:password, :password_confirmation, :current_password].collect{|p| params[:user].delete(p) }
+    end
 
+    update!
+  end
 end
