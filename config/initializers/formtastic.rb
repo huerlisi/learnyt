efa3b09b82422d2667c06e20671b0b4c60155ee3
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-10 09:05:31 +0100

    Enable lookups in formtastic, show hints before input.

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -43,12 +43,12 @@ Formtastic::SemanticFormBuilder.default_text_area_height = 5
 # errors messages. Sometimes you want the hints to be rendered first than
 # the input, in the following order: hints, input and errors. You can
 # customize it doing just as below:
-# Formtastic::SemanticFormBuilder.inline_order = [:input, :hints, :errors]
+Formtastic::SemanticFormBuilder.inline_order = [:hints, :input, :errors]
 
 # Specifies if labels/hints for input fields automatically be looked up using I18n.
 # Default value: false. Overridden for specific fields by setting value to true,
 # i.e. :label => true, or :hint => true (or opposite depending on initialized value)
-# Formtastic::SemanticFormBuilder.i18n_lookups_by_default = false
+Formtastic::SemanticFormBuilder.i18n_lookups_by_default = true
 
 # You can add custom inputs or override parts of Formtastic by subclassing SemanticFormBuilder and
 # specifying that class here.  Defaults to SemanticFormBuilder.
