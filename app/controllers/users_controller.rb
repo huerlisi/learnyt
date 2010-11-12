7112ac97c4f07cde288324e7341d2b86dbf25f12
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-12 09:07:44 +0100

    Add comments to UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
+# Users Controller
+#
+# Provides a user/account management interface.
 class UsersController < AuthorizedController
+  # Actions
   def update
     @user = User.find(params[:id])
     
+    # Don't try to update password if not provided
     if params[:user][:password].blank?
       [:password, :password_confirmation, :current_password].collect{|p| params[:user].delete(p) }
     end
