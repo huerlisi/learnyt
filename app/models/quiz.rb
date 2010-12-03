1b21e569dbb7c463604ac0de1bf322b612518dfa
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-03 11:06:15 +0100

    Destroy assigned quiz_assignments for quizzes.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -6,6 +6,8 @@ class Quiz < ActiveRecord::Base
   has_many :questions, :through => :quiz_questions, :order => 'position'
   has_many :quiz_responses
   
+  has_many :quiz_assignments, :dependent => :destroy
+  
   # Validations
   validates_presence_of :title
   
