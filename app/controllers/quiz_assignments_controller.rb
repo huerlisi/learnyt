f24cdf2a0d614325c6ff366dc5cf5b5463221919
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-03 14:32:47 +0100

    Show last response for assignment if responded, new response otherwise.

--- app/controllers/quiz_assignments_controller.rb
+++ app/controllers/quiz_assignments_controller.rb
@@ -1,5 +1,15 @@
 class QuizAssignmentsController < AuthorizedController
   # Actions
+  def show
+    @quiz_assignment = QuizAssignment.find(params[:id])
+    
+    if @quiz_assignment.responded?
+      redirect_to @quiz_assignment.quiz_responses.last
+    else
+      redirect_to new_quiz_quiz_response_path(@quiz_assignment.quiz)
+    end
+  end
+
   def create
     create! { quiz_assignments_path }
   end
