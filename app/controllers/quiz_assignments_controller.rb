016693e2af68c8b472d49fb688ff1d8477035615
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-06 12:59:40 +0100

    Use QuizAssignment.solved?, not responded?

--- app/controllers/quiz_assignments_controller.rb
+++ app/controllers/quiz_assignments_controller.rb
@@ -3,7 +3,7 @@ class QuizAssignmentsController < AuthorizedController
   def show
     @quiz_assignment = QuizAssignment.find(params[:id])
     
-    if @quiz_assignment.responded?
+    if @quiz_assignment.solved?
       redirect_to @quiz_assignment.quiz_responses.last
     else
       redirect_to new_quiz_assignment_quiz_response_path(@quiz_assignment)
--- app/models/quiz_assignment.rb
+++ app/models/quiz_assignment.rb
@@ -6,7 +6,7 @@ class QuizAssignment < ActiveRecord::Base
   belongs_to :user
   belongs_to :quiz
   has_many :quiz_responses
-  def responded?
+  def solved?
     quiz_responses.present?
   end
 
