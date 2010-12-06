eb3d4975115591efa924586e06643ace49fdde27
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-06 09:50:25 +0100

    Move logic for different linking for teacher and pupil from view to controller.

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,10 @@
 class QuizzesController < AuthorizedController
+  # Actions
+  def show
+    @quiz = Quiz.find(params[:id])
+    
+    if current_user.role?(:pupil)
+      redirect_to new_quiz_quiz_response_path(@quiz)
+    end
+  end
 end
