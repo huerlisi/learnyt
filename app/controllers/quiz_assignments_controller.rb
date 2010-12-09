7096531c99eabc71018806c2a0a3d297d2a238e4
Roman <roman.simecek@cyt.ch>
2010-12-09 14:45:12 +0100

    When quiz unsolved a notice appears when an admin wants to see it.

--- app/controllers/quiz_assignments_controller.rb
+++ app/controllers/quiz_assignments_controller.rb
@@ -6,7 +6,14 @@ class QuizAssignmentsController < AuthorizedController
   def show
     @quiz_assignment = QuizAssignment.find(params[:id])
     
-    redirect_to new_quiz_assignment_quiz_response_path(@quiz_assignment)
+    if current_user != @quiz_assignment.user and @quiz_assignment.solved?
+      redirect_to @quiz_assignment.quiz_responses.last
+    elsif current_user != @quiz_assignment.user
+      flash[:notice] = I18n.t('learnyt.quiz.not_solved')
+      redirect_to :back
+    else
+      redirect_to new_quiz_assignment_quiz_response_path(@quiz_assignment)
+    end
   end
 
   def create
