6f7f4ad01388fd1118398817eb0b03d264883a6d
Roman <roman.simecek@cyt.ch>
2010-12-09 14:31:02 +0100

    A quiz can now be solved multiple times and removed unnessesary table column.

--- app/controllers/quiz_assignments_controller.rb
+++ app/controllers/quiz_assignments_controller.rb
@@ -1,16 +1,12 @@
 class QuizAssignmentsController < AuthorizedController
   # Scopes
-  has_scope :state, :default => 'unsolved'
+  #has_scope :state, :default => 'unsolved'
   
   # Actions
   def show
     @quiz_assignment = QuizAssignment.find(params[:id])
     
-    if @quiz_assignment.solved?
-      redirect_to @quiz_assignment.quiz_responses.last
-    else
-      redirect_to new_quiz_assignment_quiz_response_path(@quiz_assignment)
-    end
+    redirect_to new_quiz_assignment_quiz_response_path(@quiz_assignment)
   end
 
   def create
