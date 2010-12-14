453df9a59510337d330d7179821790b9079ad36e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-14 07:55:42 +0100

    Use paginate in possible answer spec.

--- spec/views/possible_answers/index.html.haml_spec.rb
+++ spec/views/possible_answers/index.html.haml_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "possible_answers/index.html.haml" do
+  login_admin
+  
   before(:each) do
     assign(:possible_answers, [
       stub_model(PossibleAnswer),
       stub_model(PossibleAnswer)
-    ].stub!(:total_pages).and_return(0))
+    ].paginate(:page => 1))
   end
 
   it "renders a list of possible_answers" do
--- spec/views/questions/index.html.haml_spec.rb
+++ spec/views/questions/index.html.haml_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe "questions/index.html.haml" do
   before(:each) do
     assign(:questions, [
-      stub_model(Question),
-      stub_model(Question)
-    ])
+      Factory(:question),
+      Factory(:question)
+    ].paginate(:page => 1))
   end
 
   it "renders a list of questions" do
